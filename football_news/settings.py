ea9d1630519e55d84250b098f6c21a317c82d3f8
andrew <andrewpanjaitan@duck.com>
2025-09-11 17:45:25 +0700

    perbaikan csrf_trusted_origin di settings.py

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "andrew-sanjay-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://andrew-sanjay-footballnews.pbp.cs.ui.ac.id/"
+    "https://andrew-sanjay-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
